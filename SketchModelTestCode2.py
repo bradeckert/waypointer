ce12556448cc65565bd4911da21d903b0dc98864
bradeckert <beckert@mit.edu>
2014-10-10 10:13:28 -0400

    Test commit

--- SketchModelTestCode2.py
+++ SketchModelTestCode2.py
@@ -1,3 +1,7 @@
+
+
+
+
 #UDP Socket Test 2.0
 
 import os
@@ -17,13 +21,13 @@ ser = serial.Serial('/dev/tty.usbmodem1411', 9600, timeout=1)
 [TimeStamp, MagHead, TrueHead, HeadAcc, Lat, Long, LocAcc] = [0,0,0,0,0,0,0]
 
 
-   
+
 
 class handler(SocketServer.DatagramRequestHandler):
     def handle(self):
         newmsg = self.rfile.readline().rstrip()
         newmsgARR = [float(s) for s in newmsg.split(',')]
-        
+
 ##        with open(FILENAME, "a") as out:
 ##            m  = "Client %s said ``%s''" % (self.client_address[0], newmsg)
 ##            out.write(m + '\n')
@@ -36,7 +40,7 @@ class handler(SocketServer.DatagramRequestHandler):
         Long = float(newmsgARR[5])
         tc1=math.atan2(math.sin(Dest[1]-Long)*math.cos(Dest[0]),\
                        math.cos(Lat)*math.sin(Dest[0])-math.sin(Lat)*math.cos(Dest[0])*math.cos(Dest[1]-Long))
-        
+
         tc1 = (int((tc1 % 2*math.pi)*360/math.pi))%360
         print tc1
         ServoPos = int(360- (newmsgARR[1] - tc1))
@@ -44,8 +48,8 @@ class handler(SocketServer.DatagramRequestHandler):
         ser.write('a'+str(ServoPos)+'z')
         self.wfile.write(self.server.oldmsg)
         self.server.oldmsg = newmsg
-        
-        
+
+
 ##os.remove(FILENAME)
 s = SocketServer.UDPServer(('',PORTNO), handler)
 print "Awaiting UDP messages on port %d" % PORTNO
